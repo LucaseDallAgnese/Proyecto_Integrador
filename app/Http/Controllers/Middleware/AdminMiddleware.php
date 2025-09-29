4c3d60d204bb02aa9d70b755f627dab0cd65deb8
DanielTSAS <daniel.malaquias@educacionadventista.org.ar>
2025-09-29 20:47:43 -0300

    se agregaron y arreglaron middlewares

---
+++ app/Http/Controllers/Middleware/AdminMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+   
+    //@param \Clousure(\Illuminte\Http\Request): (\Symfony\Component\HttpFoundation\Response) ñext
+
+    public function handle(Request $request, Closure $next): Response
+{
+    if (!Auth::check() || Auth::user()->role !== 'admin'){
+        abort(403, 'Acceso no autorizado. Se requiere rol de administrador.');
+
+
+    }
+return $next($request);
+}    
+}
