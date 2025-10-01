18c0edb70bd061432575d3ecd4b1adde16d40728
DanielTSAS <daniel.malaquias@educacionadventista.org.ar>
2025-09-30 21:38:10 -0300

    intentando arreglar la vista de detalle del producto

--- resources/views/tienda/index.blade.php
+++ resources/views/tienda/index.blade.php
@@ -15,7 +15,9 @@
                         <img src="https://via.placeholder.com/120x90?text=Sin+Imagen" alt="Sin imagen" class="mb-2">
                     @endif
                 </a>
-                <div class="text-lg font-semibold mb-1">{{ $product->name }}</div>
+                <div class="text-lg font-semibold mb-1">
+                    <a href="{{route('products.shows', $product }}" class="hover:text-blue-600"> {{ $product->name }} </a>
+                </div>
                 <div class="text-gray-600 mb-1">${{ number_format($product->price, 2) }}</div>
                 <div class="text-gray-500 mb-2">Stock: {{ $product->stock }}</div>
                 <a href="{{ route('products.show', $product) }}" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">Ver</a>
