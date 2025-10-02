2930c7f8dcc8e5ac9f5839ddbb4412f5e348afb4
DanielTSAS <daniel.malaquias@educacionadventista.org.ar>
2025-10-01 21:24:53 -0300

    se agrego contenido en la carpeta contenido

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -10,22 +10,18 @@ class ProductController extends Controller
     // Listar productos
     public function index()
     {
-        $latestProducts = Product::latest()->take(8)->get();
-        
-        return view('tienda.index', ['products' => $latestProducts]);
+        $products = Product::all();
+        return view('tienda.index', compact('products'));
     }
 
     public function shop()
     {
-        
         return view('tienda.shop', compact('products'));
     }
 
     // Mostrar detalles de un producto
     public function show(Product $product)
     {
-        
         return view('tienda.show', compact('product'));
-        dd($product);
     }
 }
