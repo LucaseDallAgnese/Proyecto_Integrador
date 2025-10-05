599884392f656e4faffaa1180a5adc865fcd9740
DanielTSAS <daniel.malaquias@educacionadventista.org.ar>
2025-10-05 15:22:19 -0300

    tratando de arreglar el metdo de recuperacion de contraseña

--- app/Http/Controllers/TransaccionesController.php
+++ app/Http/Controllers/TransaccionesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class transacciones extends Controller
+class TransaccionesController extends Controller
 {
     //
 }
--- app/Models/orderItem.php
+++ app/Models/orderItem.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 // Convención: Nombre de clase y archivo en PascalCase (OrderItem)
-class orderItem extends Model
+class OrderItem extends Model
 {
     use HasFactory;
 
