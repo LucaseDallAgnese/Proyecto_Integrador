0b369bb792f70132ddb0d16fecd6b2fdc6335c44
DanielTSAS <daniel.malaquias@educacionadventista.org.ar>
2025-10-27 11:37:23 -0300

    arreglando el carrito

--- app/Http/Requests/CartAddRequest.php
+++ app/Http/Requests/CartAddRequest.php
@@ -14,6 +14,7 @@ class CartAddRequest extends FormRequest
     public function rules(): array
     {
         return[
+            'product_id' => 'required|integer|exists:products,id',
             'quantity'=> 'required|integer|min:1',
         ];
     }
