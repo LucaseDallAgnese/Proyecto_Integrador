19da8006b7d5aea6cb45ac8155935831dc609932
DanielTSAS <daniel.malaquias@educacionadventista.org.ar>
2025-10-30 15:00:58 -0300

    espero que funcione el pinche proceso de compra

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth; // <-- Asegúrate de tener este use
+use Illuminate\Support\Facades\Auth; 
 use Darryldecode\Cart\Facades\CartFacade as Cart;
 use App\Models\Order;
 use App\Models\OrderItem;
