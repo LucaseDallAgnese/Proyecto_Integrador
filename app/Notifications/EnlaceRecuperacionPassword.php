4a3fb6488d370a822a4849b5d881995498cc81e0
DanielTSAS <daniel.malaquias@educacionadventista.org.ar>
2025-10-30 13:17:51 -0300

    metodo de envio de cooreo optimizado

--- app/Notifications/EnlaceRecuperacionPassword.php
+++ app/Notifications/EnlaceRecuperacionPassword.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class EnlaceRecuperacionPassword extends Notification
+class EnlaceRecuperacionPassword extends Notification implements ShouldQueue
 {
     use Queueable;
 
